96ef5175d72758943a63aaa5e4d3a7271677a26b
Foxhole <info@foxhole.host>
2022-06-25 03:45:22 +0100

    wip

--- src/examples/demo-2.ts
+++ src/examples/demo-2.ts
@@ -3,13 +3,19 @@ import mcrx from "../index"
 const startDemo = (rootDir: string) => {
     console.log(`[MCRX/demo-2] starting demo for: ${rootDir}`)
 
-    const {files$, logs$, loggedEvents$, playerEvents$, chatEvents$} = mcrx(rootDir)
+    const {
+        files$,
+        //logs$,
+        loggedEvents$,
+        playerEvents$,
+        chatEvents$
+    } = mcrx(rootDir)
 
     const {playerJoined$, playerLeft$} = playerEvents$
     const {messageSent$} = chatEvents$
 
-    logs$.subscribe(v => console.debug(`# -> [${v.timestamp}] ${v.content}`))
-    loggedEvents$.stream$.subscribe(v => console.warn(`E -> [${v.line.timestamp}] {${v.type}} ${JSON.stringify(v.data)}`))
+    //logs$.subscribe(v => console.error(`([${v.timestamp}] ${v.content}`))
+    loggedEvents$.stream$.subscribe(v => v.type !== 'raw' ? console.warn(`E -> [${v.line.timestamp}] {${v.type}} ${JSON.stringify(v.data)}`) : undefined)
     playerJoined$.subscribe(v => console.info(`J -> [${v.timestamp}] player joined: ${v.player.name}`))
     playerLeft$.subscribe(v => console.info(`L -> [${v.timestamp}] player left: ${v.player.name}`))
     messageSent$.subscribe(v => console.info(`C -> [${v.timestamp}] ${v.message.author} sent: '${v.message.content}'`))
